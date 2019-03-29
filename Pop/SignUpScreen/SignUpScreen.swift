a14c922609578740a5aef633d862fe81002c4e07
Mason McCord <masonrileymccord@gmail.com>
2019-03-28 22:40:05 -0400

    Organization/ connecting storyboard to swift

---
+++ Pop/SignUpScreen/SignUpScreen.swift
@@ -0,0 +1,26 @@
+//
+//  HomeScreen.swift
+//  Pop
+//
+//  Created by Mason McCord on 3/11/19.
+//  Copyright © 2019 Alan Dang. All rights reserved.
+//
+import SwiftKeychainWrapper
+import Foundation
+import UIKit
+
+class SignUpPage: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+    
+    
+    
+}
