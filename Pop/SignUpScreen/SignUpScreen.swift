2f31866ad3a3b7b01f1a2a7f853abaa6c95e2d14
Mason McCord <masonrileymccord@gmail.com>
2019-04-04 10:22:20 -0400

    Connecting Storyboard/Organizing Shit

--- Pop/SignUpScreen/SignUpScreen.swift
+++ Pop/SignUpScreen/SignUpScreen.swift
@@ -1,26 +1,37 @@
 //
-//  HomeScreen.swift
+//  SignUpScreen.swift
 //  Pop
 //
-//  Created by Mason McCord on 3/11/19.
+//  Created by Mason McCord on 4/4/19.
 //  Copyright © 2019 Alan Dang. All rights reserved.
 //
-import SwiftKeychainWrapper
-import Foundation
+
 import UIKit
 
-class SignUpPage: UIViewController {
+class SignUpScreen: UIViewController {
+    
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var emailField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
     
-    
-    override func viewDidAppear(_ animated: Bool) {
-        
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-    
-    
-    
+    */
+
 }
