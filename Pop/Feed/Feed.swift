27a1b1d2b410ee955dbac25c31d01569bd5daf79
Mason McCord <masonrileymccord@gmail.com>
2019-04-07 19:06:03 -0400

    Logout Settings

--- Pop/Feed/Feed.swift
+++ Pop/Feed/Feed.swift
@@ -32,6 +32,10 @@ class Feed: UITableViewController {
         return 0
     }
 
+    
+
+    
+    
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
