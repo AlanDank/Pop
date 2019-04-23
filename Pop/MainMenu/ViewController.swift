16fc8cbdba29fac57fa05674f4c8299909b0f4e6
AlanDank <auoln.d@gmail.com>
2019-04-22 21:22:04 -0400

    Signin Works

--- Pop/MainMenu/ViewController.swift
+++ Pop/MainMenu/ViewController.swift
@@ -25,4 +25,23 @@ class ViewController: UIViewController {
         self.performSegue(withIdentifier: "toLogin", sender: self)
     }
     
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let user = Auth.auth().currentUser {
+            self.performSegue(withIdentifier: "toFeed", sender: self)
+        }
+    }
 }
