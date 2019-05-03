f6b0d79b3b5965fc4be2b69a9f3395b784d11786
Mason McCord <masonrileymccord@gmail.com>
2019-05-02 22:17:24 -0400

    Setting up tableview

--- Pop/Feed/FeedViewController.swift
+++ Pop/Feed/FeedViewController.swift
@@ -9,26 +9,57 @@
 import UIKit
 import Firebase
 
-class FeedViewController: UIViewController {
-
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    var tableview:UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableview = UITableView(frame: view.bounds, style: .plain)
+        tableview.backgroundColor = UIColor.blue
+        let cellNib = UINib(nibName: "PostTableViewCell", bundle: nil)
+        tableview.register(cellNib, forCellReuseIdentifier: "postCell")
+        view.addSubview(tableview)
+        
+        
+        var layoutGuide:UILayoutGuide!
+        
+        if #available(iOS 11.0, *){
+        layoutGuide = view.safeAreaLayoutGuide
+        } else {
+            layoutGuide = view.layoutMarginsGuide
+        }
+        
+        tableview.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor).isActive = true
+        tableview.topAnchor.constraint(equalTo: layoutGuide.topAnchor).isActive = true
+        tableview.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor).isActive = true
+        tableview.bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor).isActive = true
+        
+        tableview.delegate = self
+        tableview.dataSource = self
+        tableview.reloadData()
+        
+        
         // Do any additional setup after loading the view.
     }
     @IBAction func handleLogout(_ sender:Any) {
         try! Auth.auth().signOut() 
         
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableview.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as! PostTableViewCell
+        return cell
+    }
+    
+    
 }
---
+++ Pop/Feed/PostTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PostTableViewCell.swift
+//  Pop
+//
+//  Created by Mason McCord on 5/2/19.
+//  Copyright © 2019 Alan Dang. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
