06946e7c2e6c83b60b1fa52035d0a389d524210b
AlanDank <auoln.d@gmail.com>
2019-05-03 13:31:43 -0400

    Revert "Merge branch 'AlanMaster2' of https://github.com/AlanDank/Pop into AlanMaster2"
    This reverts commit 58e6cb1bf342ad556bff8ef54726b47547b4249e, reversing
    changes made to deb77a44e8efe05debbb8b518646ea3b0ac6f19c.

--- Pop/Feed/FeedViewController.swift
+++ Pop/Feed/FeedViewController.swift
@@ -9,66 +9,26 @@
 import UIKit
 import Firebase
 
-class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    
-    var tableview:UITableView!
-    
-    var posts = [
-        Post(id:"1", author: "Donald Trump", text: "Bigly"),
-        Post(id:"2", author: "Luke Skywalker", text: "I did not like the last jedi because I did not get to use my awesome jedi powers"),
-        Post(id:"3", author: "Drizzy Drake", text: "Ridin through the 6 with my woes")
-        
-    ]
-    
+class FeedViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableview = UITableView(frame: view.bounds, style: .plain)
-        tableview.backgroundColor = UIColor.blue
-        let cellNib = UINib(nibName: "PostTableViewCell", bundle: nil)
-        tableview.register(cellNib, forCellReuseIdentifier: "postCell")
-        view.addSubview(tableview)
-        
-        
-        var layoutGuide:UILayoutGuide!
-        
-        if #available(iOS 11.0, *){
-        layoutGuide = view.safeAreaLayoutGuide
-        } else {
-            layoutGuide = view.layoutMarginsGuide
-        }
-        
-        tableview.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor).isActive = true
-        tableview.topAnchor.constraint(equalTo: layoutGuide.topAnchor).isActive = true
-        tableview.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor).isActive = true
-        tableview.bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor).isActive = true
-        
-        tableview.delegate = self
-        tableview.dataSource = self
-        tableview.tableFooterView = UIView()
-        tableview.reloadData()
-        
-        
+
         // Do any additional setup after loading the view.
     }
     @IBAction func handleLogout(_ sender:Any) {
         try! Auth.auth().signOut() 
         
     }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return posts.count
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableview.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as! PostTableViewCell
-        cell.set(post: posts[indexPath.row])
-        return cell
-    }
-    
-    
+    */
+
 }
